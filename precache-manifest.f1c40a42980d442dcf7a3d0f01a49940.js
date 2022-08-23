7db937577c423147155fee9754c1eb5c862fa425
sarthakkr00005@gmail.com <sarthakkr00005@gmail.com>
2022-08-23 23:55:32 +0530

    Updates

---
+++ precache-manifest.f1c40a42980d442dcf7a3d0f01a49940.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a825a8c61b9cb1cb803aed12f8a61f8",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "f688d5f2256f64470d59",
+    "url": "/KeeperApp/static/js/2.aa89413b.chunk.js"
+  },
+  {
+    "revision": "4b56d93c478f64733ad0",
+    "url": "/KeeperApp/static/js/main.1fb37fd3.chunk.js"
+  },
+  {
+    "revision": "f87f7f290d4c0085841d",
+    "url": "/KeeperApp/static/js/runtime-main.9f8bc1f0.js"
+  }
+]);
